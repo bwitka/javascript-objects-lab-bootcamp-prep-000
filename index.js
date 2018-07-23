37bd48d3757c67e2a2dd0898f73fe7299e4057fc
BEAU WITKA <bwitka@gmail.com>
2018-07-23 00:26:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+  var newObject = Object.assign({}, object);
+  delete newObject.key;
+  return object;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
